7c5235540161195573672491cba80effc6f51efb
root <615810790@qq.com>
2017-12-21 22:53:27 +0800

    Add assert api and fake net connection struct

---
+++ nsqio/nsq/internal/test/assertions.go
@@ -0,0 +1,48 @@
+package test
+
+import (
+    "path/filepath"
+    "reflect"
+    "runtime"
+    "testing"
+)
+
+func Equal(t *testing.T, expected, actual interface{}) {
+    if !reflect.DeepEqual(expected, actual) {
+        _, file, line, _ := runtime.Caller(1)
+        t.Logf("\033[31m%s:%d:\n\n\t %#v(expected)\n\n\t!= %#v (actual)\033[39m\n\n",
+        filepath.Base(file), line, expected, actual)
+        t.FailNow()
+    }
+}
+
+func NotEqual(t *testing.T, expected, actual interface{}) {
+    if reflect.DeepEqual(expected, actual) {
+        _, file, line, _ := runtime.Caller(1)
+        t.Logf("\033[31m%s:%d:\n\n\tnexp:%#v\n\n\tgot: %#v\033[39m\n\n",
+        filepath.Base(file), line, expected, actual)
+        t.FailNow()
+    }
+}
+
+func Nil(t *testing.T, object interface{}) {
+    if !isNil(object) {
+        t.Logf("\033[31m%s:%d:\n\n\tnexp:%#v\n\n\tgot: %#v\033[39m\n\n",
+            filepath.Base(file), line, object)
+            t.FailNow()
+    }
+}
+
+func isNil(object interface{}) bool {
+    if object == nil {
+        return true
+    }
+
+    value := reflect.ValueOf(object)
+    kind := value.Kind()
+    if kind >= reflect.Chan && kind <= reflect.Slice && value.IsNil() {
+        return true
+    }
+
+    return false
+}
